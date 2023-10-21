47a034031740cde5cc6c077fa2ee0df40298ae95
RioSakao <skoqush@gmail.com>
2023-10-21 12:00:51 -0700

    Created post() in views.py to handles the post request and returns back queries within 150 km of the user's location

--- nearbyshops/tourism/views.py
+++ nearbyshops/tourism/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib.gis.measure import D
+from django.contrib.gis.db.models import PointField
 
 from .models import Tourism
 from .serializers import TourismSerializer
@@ -10,3 +12,17 @@ class TourismViews(APIView):
         TourismModel = Tourism.objects.all()
         serializer = TourismSerializer(TourismModel, many=True)
         return Response({"TourismModel": serializer.data})
+
+    def post(self,request,format=None):
+        search_query = (request.data).split()
+        lat = int(search_query[0])
+        long = int(search_query[1])
+        base_point = PointField(lat,long)
+        tourism = Tourism.objects
+        #nearby queries within 150 km
+        matched_data = tourism.filter(
+            location__distance_lte=(
+                base_point, D(km=150))
+                ).distance(base_point).order_by('distance')
+            
+        return Response(data=matched_data)
